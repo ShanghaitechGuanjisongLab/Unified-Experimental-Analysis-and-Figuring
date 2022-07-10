5278dd92a69465e3d26d9d9349d18e806b25e8ac
埃博拉酱 <vhtmfu@outlook.com>
2022-07-10 11:49:01 +0800

    UniExpException依赖MATLAB.Lang.IEnumerableException

--- +UniExp/UniExpException.m
+++ +UniExp/UniExpException.m
@@ -1,11 +1,6 @@
-classdef UniExpException
+classdef UniExpException<MATLAB.Lang.IEnumerableException
 	enumeration
 		Wrong_number_of_arguments
 		Image_size_does_not_match
-	end
-	methods
-		function Throw(obj)
-			error(sprintf('UniExp:%s',obj),string(obj));
-		end
 	end
 end
--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -1,5 +1,5 @@
 function V = Version
-V.Me='1.0.1';
-V.MatlabExtension=MATLAB.Version;
+V.Me='1.0.2';
+V.ParallelComputing=ParallelComputing.Version;
 V.Image5D=Image5D.Version;
-V.MATLAB='R2020a';
+V.MATLAB='R2022b';
