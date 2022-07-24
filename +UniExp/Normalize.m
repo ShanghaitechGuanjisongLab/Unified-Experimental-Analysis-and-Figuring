3871415be5242897ddcc11f2cd253e8f46f138b7
埃博拉酱 <vhtmfu@outlook.com>
2022-07-24 12:29:21 +0800

    v1.4.0 修复PcaDemo表格构造错误 对数归一化改用log2 依赖统计和机器学习工具箱

--- +UniExp/Normalize.m
+++ +UniExp/Normalize.m
@@ -8,8 +8,8 @@ classdef Normalize
 		%将基线均值记为F0，计算F/F0-1为归一化数据
 		dFdF0
 
-		%将基线均值记为F0，计算log(F/F0)为归一化数据
-		lnFdF0
+		%将基线均值记为F0，计算log2(F/F0)为归一化数据
+		log2FdF0
 
 		%将基线均值记为μ，标准差记为σ，计算(F-μ)/σ为归一化数据
 		ZScore
