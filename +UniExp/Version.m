200cfefd138c53934b84e0ea24e6b322317323b9
埃博拉酱 <vhtmfu@outlook.com>
2022-07-25 11:43:36 +0800

    LinearPca：错误信息更详细，显示出错在哪条线

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -1,5 +1,6 @@
 function V = Version
-V.Me='1.5.0';
+V.Me='1.5.1';
 V.ParallelComputing=ParallelComputing.Version;
+V.GlobalOptimization=GlobalOptimization.Version;
 V.Image5D=Image5D.Version;
 V.MATLAB='R2022b';
