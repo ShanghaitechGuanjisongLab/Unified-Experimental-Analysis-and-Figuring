6798f15ba0b1f5c41a976bdf69047f057dadd73a
埃博拉酱 <vhtmfu@outlook.com>
2022-11-07 08:48:02 +0800

    v8.0.1

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -1,5 +1,5 @@
 function V = Version
-V.Me='8.0.0';
+V.Me='8.0.1';
 V.ParallelComputing=ParallelComputing.Version;
 V.GlobalOptimization=GlobalOptimization.Version;
 V.ImageProcessing=ImageProcessing.Version;
