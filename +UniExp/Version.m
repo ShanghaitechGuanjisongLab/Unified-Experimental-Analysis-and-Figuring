a8828359d1d38e0cd62d732c85fdb97c78817c19
埃博拉酱 <33191697+Silver-Fang@users.noreply.github.com>
2022-11-11 09:35:43 +0800

    版本号8.1.0

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -1,5 +1,5 @@
 function V = Version
-V.Me='8.0.1';
+V.Me='8.1.0';
 V.ParallelComputing=ParallelComputing.Version;
 V.GlobalOptimization=GlobalOptimization.Version;
 V.ImageProcessing=ImageProcessing.Version;
