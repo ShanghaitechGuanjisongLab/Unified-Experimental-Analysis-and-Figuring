f09628dab4f9234c96899afbf1e1a3394c2fc523
埃博拉酱 <vhtmfu@outlook.com>
2023-02-15 20:17:15 +0800

    因为有不兼容性更改，大版本号提升

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -1,5 +1,5 @@
 function V = Version
-V.Me='10.1.0';
+V.Me='11.0.0';
 V.ParallelComputing=ParallelComputing.Version;
 V.GlobalOptimization=GlobalOptimization.Version;
 V.ImageProcessing=ImageProcessing.Version;
