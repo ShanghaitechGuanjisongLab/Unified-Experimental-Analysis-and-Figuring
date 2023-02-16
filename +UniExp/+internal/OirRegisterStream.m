b19f578fe9bcc58611395e96788bee50318944fa
埃博拉酱 <33191697+Silver-Fang@users.noreply.github.com>
2023-02-16 10:09:12 +0800

    磁盘满时允许修改输出目录继续

--- +UniExp/+internal/OirRegisterStream.m
+++ +UniExp/+internal/OirRegisterStream.m
@@ -4,7 +4,33 @@ classdef OirRegisterStream<ParallelComputing.BlockRWStream
 			Index=obj.ObjectsRead+1;
 			if Index<=obj.NumObjects
 				fprintf('%s 文件%u/%u：%s\n',datetime,Index,obj.NumObjects,obj.RWObjects(Index).OirPaths);
-				obj.NextObject@ParallelComputing.BlockRWStream;
+				while true
+					try
+						obj.NextObject@ParallelComputing.BlockRWStream;
+						break
+					catch ME
+						if ME.identifier=="Image5D:Image5DException:Tiff_file_creation_failed")&&ME.Detail==MATLAB.WindowsErrorCode.ERROR_DISK_FULL
+							NewOutput=input('输出目录磁盘已满。直接回车以选择新的输出目录，或输入c以取消本次任务。',"s");
+							if NewOutput=="c"
+								ME.rethrow;
+							end
+							NewOutput=uigetdir;
+							while isequal(NewOutput,0)
+								NewOutput=input('未选择新目录。直接回车以选择新的输出目录，或输入c以取消本次任务。',"s");
+								if NewOutput=="c"
+									ME.rethrow;
+								end
+								NewOutput=uigetdir;
+							end
+							for I=Index:obj.NumObjects
+								[~,Filename]=fileparts(obj.RWObjects(I).TiffPaths);
+								obj.RWObjects(I).TiffPaths=fullfile(NewOutput,Filename+".tif");
+							end
+						else
+							ME.rethrow;
+						end
+					end
+				end
 			end
 		end
 	end
