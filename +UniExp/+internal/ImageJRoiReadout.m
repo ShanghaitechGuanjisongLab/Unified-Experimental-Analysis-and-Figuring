8c038fa6c0ae2b81331da7607de527b2253768fb
埃博拉酱 <33191697+Silver-Fang@users.noreply.github.com>
2023-02-17 17:13:43 +0800

    v12.0.1 ImageJRoiReadout：修复RoiSet无法获取nPosition的问题 BatchTiffMeasure：修复Cxy维度错误 RoiRegister：imwarp颠倒XY

--- +UniExp/+internal/ImageJRoiReadout.m
+++ +UniExp/+internal/ImageJRoiReadout.m
@@ -1,8 +1,9 @@
 function [Cxy,Rxy,Zs] = ImageJRoiReadout(RoiSet)
-RoiSet=UniExp.internal.ReadImageJROI(RoiSet);
-RoiSet=struct2table(vertcat(RoiSet{:})).vnRectBounds;
-Cxy=([RoiSet(:,2)+RoiSet(:,4),RoiSet(:,1)+RoiSet(:,3)]+1)/2;
-Rxy=[RoiSet(:,4)-RoiSet(:,2),RoiSet(:,3)-RoiSet(:,1)]/2;
+RoiSet=ReadImageJROI(RoiSet);
+RoiSet=struct2table(vertcat(RoiSet{:}));
+Positions=RoiSet.vnRectBounds;
+Cxy=([Positions(:,2)+Positions(:,4),Positions(:,1)+Positions(:,3)]+1)/2;
+Rxy=[Positions(:,4)-Positions(:,2),Positions(:,3)-Positions(:,1)]/2;
 if RoiSet.nPosition(1)
 	Zs=RoiSet.nPosition;
 else
