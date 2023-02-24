8d0a46994a2494295b24832c7295e69ed5b0ab05
埃博拉酱 <vhtmfu@outlook.com>
2023-02-24 12:33:36 +0800

    自动检查更新

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -5,3 +5,20 @@ V.GlobalOptimization=GlobalOptimization.Version;
 V.ImageProcessing=ImageProcessing.Version;
 V.Image5D=Image5D.Version;
 V.MATLAB='R2022b';
+persistent NewVersion
+if isempty(NewVersion)
+	try
+		NewVersion=webread('https://github.com/ShanghaitechGuanjisongLab/Unified-Experimental-Analysis-and-Figuring/releases');
+	catch ME
+		if any(ME.identifier==["MATLAB:webservices:ConnectionRefused","MATLAB:webservices:UnknownHost"])
+			NewVersion=[];
+			return;
+		else
+			ME.rethrow;
+		end
+	end
+	NewVersion=char(htmlTree(NewVersion).findElement('section:first-child span.wb-break-all').extractHTMLText);
+	if ~strcmp(NewVersion(2:end),V.Me)
+		disp(['统一实验分析作图' NewVersion '已发布，<a href="https://github.com/ShanghaitechGuanjisongLab/Unified-Experimental-Analysis-and-Figuring/releases">立即更新</a>']);
+	end
+end
