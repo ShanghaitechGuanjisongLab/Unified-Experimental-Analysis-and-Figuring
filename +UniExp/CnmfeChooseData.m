6789f9cc434c48c11d145ffc32e456098f4135fb
张天夫 <@>
2025-02-11 09:49:09 +0800

    跑通CNMFE

--- +UniExp/CnmfeChooseData.m
+++ +UniExp/CnmfeChooseData.m
@@ -1,4 +1,4 @@
-function [data,Ysiz,d1,d2,numFrame]=CnmfeChooseData(nam)
+function [data,Ysiz,d1,d2,numFrame,dir_nm,file_nm]=CnmfeChooseData(nam)
 	Reader=Image5D.OmeTiffRWer.OpenRead(nam);
 	data=struct;
 	data.Y=Reader.ReadPixels;
@@ -7,6 +7,6 @@ function [data,Ysiz,d1,d2,numFrame]=CnmfeChooseData(nam)
 	d1 = Ysiz(1);   %height
 	d2 = Ysiz(2);   %width
 	numFrame = Ysiz(3);    %total number of frames
-	
+	[dir_nm,file_nm]=fileparts(nam);
 	fprintf('\nThe data has been mapped to RAM. It has %d X %d pixels X %d frames. \nLoading all data requires %.2f GB RAM\n\n', d1, d2, numFrame, prod(Ysiz)*8/(2^30));
 end
