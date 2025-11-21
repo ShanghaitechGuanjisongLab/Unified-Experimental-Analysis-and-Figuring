c9c152f514ad4ff8b178f66341efc44e81fe785d
埃博拉酱 <vhtmfu@outlook.com>
2025-11-21 10:03:34 +0800

    更新依赖版本

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -6,8 +6,8 @@ V.ImageProcessing='3.6.1';
 V.Image5D='3.2.2';
 V.TextAnalytics='1.0.3';
 V.ComputerVision='1.1.1';
-V.MatlabExtension='v19.8.0';
-V.MATLAB='R2025a';
+V.MatlabExtension='v19.12.0';
+V.MATLAB='R2026a';
 persistent NewVersion
 if isempty(NewVersion)
 	warning('off','TextAnalyticsException:Thread_parallelism_not_supported');
