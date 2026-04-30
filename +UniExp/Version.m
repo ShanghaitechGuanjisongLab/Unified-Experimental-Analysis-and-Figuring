c426b1eec9930a872442f18551053fb06d3137d7
埃博拉酱 <vhtmfu@outlook.com>
2026-04-30 10:39:07 +0800

    v20.10.0

--- +UniExp/Version.m
+++ +UniExp/Version.m
@@ -1,12 +1,12 @@
 function V = Version
-V.Me='v20.9.0';
+V.Me='v20.10.0';
 V.ParallelComputing='v8.1.4';
 V.GlobalOptimization='v3.1.2';
 V.ImageProcessing='v3.10.0';
 V.Image5D='v3.3.0';
 V.TextAnalytics='v1.0.3';
 V.ComputerVision='v1.1.1';
-V.MatlabExtension='v20.0.0';
+V.MatlabExtension='v20.0.4';
 V.MATLAB='R2026a';
 persistent NewVersion
 if isempty(NewVersion)
